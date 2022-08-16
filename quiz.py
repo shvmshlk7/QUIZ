9ca8247639ce51fa4f9a06cad4376207cb26fd00
SHIVAM SHELKE <103065763+shvmshlk7@users.noreply.github.com>
2022-08-16 11:42:44 +0530

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,39 @@
+print("!!Welcome to my Quiz!!")
+
+playing = input("Do you want to play :- ")
+
+if playing != "yes":
+    quit()
+
+print("Okay!!! Lets Play :) ")
+
+Q1 = input("Who is our president ? ")
+if Q1 == "draupadi murmu":
+    print("Correct")
+else:
+    print("Incorrect")
+
+Q2 = input("How many days are in a leap year ? ")
+if Q2 == "366":
+    print("Correct")
+else:
+    print("Incorrect")
+
+Q3 = input("Who discovered X-Rays ? ")
+if Q3 == "Issac Newton":
+    print("Correct")
+else:
+    print("Incorrect")
+
+Q4 = input("Which is the smallest district of india ? ")
+if Q4 == "Mahe":
+    print("Correct")
+else:
+    print("Incorrect")
+
+Q5 = input("Which is the longest National highway in India ? ")
+if Q5 == "NH-44":
+    print("Correct")
+else:
+    print("Incorrect")
+
